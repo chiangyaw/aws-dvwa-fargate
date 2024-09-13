6eb31ce5240b2d043557cc4ed3621e9f446f6e0c
bechong <bechong@paloaltonetworks.com>
2024-09-13 17:11:04 +0800

    revised file

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ resource "aws_ecs_cluster" "dvwa_cluster" {
 
 # IAM Role for ECS Task Execution
 resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "becsTaskExecutionRole"
+  name = "ecsTaskExecutionRole"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
